71b663f8aca10760bf980d181ca60cc4b50828eb
hjltu <hjltu@ya.ru>
2018-12-21 22:15:18 +0500

    Update mylib.py

--- mylib.py
+++ mylib.py
@@ -31,7 +31,7 @@ def get_serial():
             for line in f:
                 if 'Serial' in line:
                     return str(line[10:26])
-            return "NA"
+            return "NoSerial"
     except Exception as e:
         return "ERR:" + str(e)
 
@@ -116,31 +116,6 @@ def set_time():
         return my_date
     except Exception as e:
         return "ERR:" + str(e)
-
-def my_log(file,msg,show=0):
-    my_date = datetime.date.today().strftime(".%d-%b-%Y")
-
-    # absolute path to this file
-    full_path = os.path.realpath(__file__)
-    path=os.path.split(full_path)
-    # path: file[0], filename: file[1]
-    file=os.path.split(file)
-
-    logger = logging.getLogger(__name__)
-    #logger.setLevel(logging.INFO)
-    # create a file handler
-    handler = logging.FileHandler(path[0] + "/" + file[1] + my_date + '.log')
-    handler.setLevel(logging.INFO)
-    # create a logging format
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    # add the handlers to the logger
-    if (logger.hasHandlers()):
-        logger.handlers.clear()
-    logger.addHandler(handler)
-    logger.info(file[1] + " " + str(msg))
-    if show == 1:
-        print(msg)
 
 # check 'reboot' command with retain=True
 def my_retain_check(command,retain):
