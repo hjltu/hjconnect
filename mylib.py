b1d843e692a4bbecd70d32ddedda327e17531d96
hjltu <hjltu@ya.ru>
2019-01-09 19:23:38 +0500

    Add files via upload

--- mylib.py
+++ mylib.py
@@ -4,19 +4,25 @@
     libs for hjconnect.py
 """
 
-import os, subprocess, json
+import os
+import subprocess
+import json
 import urllib.request
-import psutil 
-import time, datetime
+import psutil
+import time
+import datetime
 import base64
 import hashlib
+
 
 def my_json(payload):
     return json.dumps(payload)  # object2string
 
+
 def my_exit(err):
     os._exit(err)
     os.kill(os.getpid)
+
 
 def get_serial():
     # Extract serial from cpuinfo file for RaspberryPI
@@ -29,14 +35,16 @@ def get_serial():
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def get_uptime():
     try:
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = float(f.readline().split()[0])
-            uptime = str(datetime.timedelta(seconds = uptime_seconds))
+            uptime = str(datetime.timedelta(seconds=uptime_seconds))
             return uptime
     except Exception as e:
         return "ERR:" + str(e)
+
 
 def get_total_mem():
     try:
@@ -45,22 +53,25 @@ def get_total_mem():
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def get_used_mem():
     try:
         ram = psutil.virtual_memory()
-        ram_used = ram.used# / 2**20
-        #ram_percent_used = ram.percent
+        ram_used = ram.used  # / 2**20
+        # ram_percent_used = ram.percent
         return ram_used
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def get_free_space():
     try:
         disk = psutil.disk_usage('/')
-        disk_free = disk.free #/ 2**30
+        disk_free = disk.free  # / 2**30
         return disk_free
     except Exception as e:
         return "ERR:" + str(e)
+
 
 def get_sd_size():
     try:
@@ -68,15 +79,18 @@ def get_sd_size():
         disk_total = disk.total
         return disk_total
     except Exception as e:
-        return "ERR:" + str(e)  
+        return "ERR:" + str(e)
+
 
 def get_cpu_temp():
     try:
-        res=os.popen("vcgencmd measure_temp").readline().replace("\'C\n","")
-        l=res.split('=')
+        res = os.popen(
+            "vcgencmd measure_temp").readline().replace("\'C\n", "")
+        l = res.split('=')
         return l[1]
     except Exception as e:
         return "ERR:" + str(e)
+
 
 def get_cpu_usage():
     try:
@@ -84,42 +98,45 @@ def get_cpu_usage():
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def get_ip_addr():
     """ all ip addresses"""
     try:
-        res=subprocess.check_output(['hostname','-I'])
-        return res.decode('utf-8').replace(" \n","")
+        res = subprocess.check_output(['hostname', '-I'])
+        return res.decode('utf-8').replace(" \n", "")
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def get_public_ip():
     try:
-        res=urllib.request.urlopen('https://httpbin.org/ip')
-        j=json.loads(res.read().decode('utf-8'))
+        res = urllib.request.urlopen('https://httpbin.org/ip')
+        j = json.loads(res.read().decode('utf-8'))
         return j['origin']
     except Exception as e:
         return "ERR:" + str(e)
 
+
 def set_time():
     try:
-        res=urllib.request.urlopen('https://now.httpbin.org')
-        j=json.loads(res.read().decode('utf-8'))
-        my_date=str(j['now']['rfc2822'])
-        #print(my_date[5:-4])
+        res = urllib.request.urlopen('https://now.httpbin.org')
+        j = json.loads(res.read().decode('utf-8'))
+        my_date = str(j['now']['rfc2822'])
+        # print(my_date[5:-4])
         # set UTC time
-        #os.system('sudo date -s "%s"' % str(my_date[5:-4]).upper())
+        # os.system('sudo date -s "%s"' % str(my_date[5:-4]).upper())
         return my_date
     except Exception as e:
         return "ERR:" + str(e)
 
-def my_log(msg,show=0):
+
+def my_log(msg, show=0):
     logdir = "log"
     try:
         if not os.path.exists(logdir):
             os.makedirs(logdir)
     except:
         return
-
 
     mydate = datetime.date.today().strftime("%d-%b-%Y")
     mytime = time.strftime("%d-%b-%y_%H:%M:%S")
@@ -133,17 +150,20 @@ def my_log(msg,show=0):
     if show == 1:
         print(msg)
 
+
 # check 'reboot' command with retain=True
-def my_retain_check(command,retain):
-    if retain == True:
-        status.my_log(__file__,'ERR: ' + command + ' + retain')
+def my_retain_check(command, retain):
+    if retain is True:
+        status.my_log(__file__, 'ERR: ' + command + ' + retain')
         return True
     else:
         return False
 
+
 def bin2str_file(file):
     with file:
         return base64.b64encode(file.read())
+
 
 def my_md5(fname):
     hash_md5 = hashlib.md5()
@@ -151,4 +171,3 @@ def my_md5(fname):
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
-
